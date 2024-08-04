bd00bb75485b075e3f6a975c3bf5a17cd28c5046
Hoon <ehdgns730@gmail.com>
2024-08-05 00:01:04 +0900

    240804 YDH detail script in process

--- scripts/detail.js
+++ scripts/detail.js
@@ -1,17 +1,72 @@
+// Img-slider
+const moveSlide = (num) => {
+    const imgWrapper = document.querySelector(".img-wrapper");
+    const slideWidth = document.querySelector(".img-slide").offsetWidth;
 
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    imgWrapper.style.transform = `translateX(-${slideWidth * num}px)`
+    currentIndex = num;
+    pagerActive();
+};
+
+const slideCount = document.querySelectorAll(".img-slide").length;
+
+let currentIndex = 0;
+
+const moveLeft = () => {
+    let prevIndex = (currentIndex - 1) % slideCount;
+    if(currentIndex === 0) prevIndex = slideCount - 1;
+    moveSlide(prevIndex);
+};
+
+const moveRight = () => {
+    let nextIndex = (currentIndex + 1) % slideCount;
+    if(currentIndex === slideCount - 1) nextIndex = 0;
+    moveSlide(nextIndex);
+}
+
+const imgPrev = document.querySelector(".img-prev");
+const imgNext = document.querySelector(".img-next");
+
+imgPrev.addEventListener("click", moveLeft);
+imgNext.addEventListener("click", moveRight);
+
+const pagers = document.querySelectorAll(".img-pager");
+
+pagers.forEach((pager, index) => {
+    pager.addEventListener("click", () => {
+        moveSlide(index);
+        pagerActive();
+    });
+});
+
+const pagerActive = () => {
+    pagers.forEach((item) => {
+        item.addEventListener("click", function() {
+            pagers.forEach((sibling) => {
+                if(sibling !== item) {
+                    sibling.classList.remove("active");
+                }
+            });
+            this.classList.add("active");
+        });
+    });
+};
+
+
+// Map API
+const mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
 // 마커가 표시될 위치입니다 
-var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
+const markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
 
 // 마커를 생성합니다
-var marker = new kakao.maps.Marker({
+const marker = new kakao.maps.Marker({
     position: markerPosition
 });
 
