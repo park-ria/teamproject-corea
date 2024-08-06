1aae0e3cad5b52bb9111c2a158dfbd551d8b5538
hoon730 <ehdgns730@gmail.com>
2024-08-07 08:40:08 +0900

    240807 YDH pulling home's data

--- scripts/script.js
+++ scripts/script.js
@@ -93,9 +93,17 @@ items.forEach((item) => {
 });
 
 // iframe
+// function calcHeight() {
+//   const theHeight =
+//     document.querySelector("#contentFrame").contentWindow.document.body
+//       .scrollHeight;
+//   document.querySelector("#contentFrame").height = theHeight;
+// }
+
 function calcHeight() {
   const theHeight =
     document.querySelector("#contentFrame").contentWindow.document.body
       .scrollHeight;
-  document.querySelector("#contentFrame").height = theHeight;
+  document.querySelector("#contentFrame").height = theHeight+100;
+  console.log(theHeight+100);
 }
