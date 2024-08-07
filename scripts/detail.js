22b0dfc7b6575bd070c247a7a01cfee423bcafb8
hoon730 <ehdgns730@gmail.com>
2024-08-07 19:05:58 +0900

    240807 YDH json script in process

--- scripts/detail.js
+++ scripts/detail.js
@@ -62,12 +62,12 @@ fetch(JoonggoInfo).then((response) => response.json()).then((JoongoData) => {
 
         // Making User-img
         const userImg = document.querySelector(".user-img");
-        const img = document.createElement("img");
-        const src = document.createAttribute("src")
+        const userImgTag = document.createElement("img");
+        const userImgSrc = document.createAttribute("src")
 
-        src.value = product.product_img_path;
-        img.setAttributeNode(src);
-        userImg.appendChild(img);
+        userImgSrc.value = product.product_img_path;
+        userImgTag.setAttributeNode(userImgSrc);
+        userImg.appendChild(userImgTag);
 
         // Making User-id 
         const userId = document.querySelector(".user-id");
@@ -99,10 +99,41 @@ fetch(JoonggoInfo).then((response) => response.json()).then((JoongoData) => {
         // Making Map-area
         const mapArea = document.querySelector(".desc-map");
         mapArea.innerText = `- ${product.point}`;
-    }
-        
-});
 
+        // Making ItemIfo-detail
+        const itemIfoDetail = document.querySelector(".itemIfo-detail");
+        itemIfoDetail.innerHTML = 
+        `
+        <p>${product.product_description}</p>
+        <h6>연관 검색어</h6>
+        <div class="relatedWords">
+            <span>#루이비통</span>
+            <span>#트랙수트</span>
+            <span>#메쉬</span>
+        </div>
+        `;
+
+        // Making Store-info
+        const storeImg = document.querySelector(".info-img");
+        const storeImgTag = document.createElement("img");
+        const storeImgSrc = document.createAttribute("src")
+
+        storeImgSrc.value = product.product_img_path;
+        storeImgTag.setAttributeNode(storeImgSrc);
+        storeImg.appendChild(storeImgTag);
+
+        // Making Store-name 
+        const storeName = document.querySelector(".name");
+        storeName.innerText = product.product_store;
+
+        // Making Item-info
+        const itemInfos = document.querySelectorAll(".item-info");
+        console.log(itemInfos)
+        itemInfos.forEach((item) => {
+            item.innerHTML = ``
+        });
+    }
+});
 // Img-slider
 let currentIndex = 0;
 
@@ -187,3 +218,24 @@ marker.setMap(map);
 
 // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
 // marker.setMap(null);    
+
+
+// store-btns 클릭시 store-contents opacity 변화
+const storeBtns = document.querySelectorAll(".store-btns ul li");
+
+const storeContents = document.querySelectorAll(".store-contents > div");
+
+storeBtns.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        storeContents.forEach((content) => {
+            content.classList.remove("active");
+        });
+        storeContents[index].classList.add("active");
+    });
+});
+
+
+
+
+
+
