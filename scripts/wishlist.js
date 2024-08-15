091b2bd879d77f1134e149755651416508da7080
박리아 <danny6217@gmail.com>
2024-08-15 23:42:32 +0900

    240815/park/장바구니 데이터 넣기

--- scripts/wishlist.js
+++ scripts/wishlist.js
@@ -23,6 +23,104 @@ wishlistTabButton.forEach((btn) => {
   });
 });
 
+let wishItemArr = [];
+
+// 데이터 추가
+fetch("../db.json")
+  .then((response) => response.json())
+  .then((jsonData) => {
+    jsonData.data.forEach((product, index) => {
+      document.querySelector(".wishItemList").innerHTML += `
+      <li>
+        <input type="checkbox" name="checkWishItem" id="wishItem${
+          index + 1
+        }" value="wishItem${index + 1}" />
+        <div class="wishItem">
+          <div class="wishItemImgWrapper">
+            <a href="#" class="wishItemViewMore">
+              <img
+                src="../${product.image_path}"
+                alt="product_image"
+              />
+            </a>
+            <button class="wishHeart">
+              <i class="fa-solid fa-heart"></i>
+            </button>
+          </div>
+          <div class="wishItemDescGroup">
+            <div class="wishItemDesc">
+              <a href="#" class="wishItemViewMore">
+                <span class="wishItemInfo">
+                  <span class="wishItemTitleBox">
+                    <p class="wishItemSellerName">${
+                      product.detail.product_store
+                    }</p>
+                    <p class="wishItemName">
+                      ${product.title}
+                    </p>
+                  </span>
+                  <span class="wishItemPriceBox">
+                    <span class="wishItemPriceInfo">
+                      <p class="wishItemPrice">
+                        <b>${product.price.slice(0, -1)}</b>원
+                      </p>
+                      ${
+                        product.pay_flag > 0
+                          ? "<span class='pay'>pay</span>"
+                          : ""
+                      }
+                    </span>
+                    <span class="wishItemShippingInfo">
+                      <p class="wishItemShippingCharge">
+                        ${
+                          product.detail.delivery_charge === "-"
+                            ? "직거래"
+                            : `배송비 ${product.detail.delivery_charge}`
+                        }
+                      </p>
+                      ${
+                        product.point === ""
+                          ? ""
+                          : `<span class="wishItemPlace"><i class="fa-solid fa-location-dot"></i> ${product.point}</span>`
+                      }
+                      
+                    </span>
+                  </span>
+                </span>
+              </a>
+              <span class="wishItemButtons">
+                <button class="wishItemChatButton">채팅하기</button>
+                ${
+                  product.pay_flag > 0
+                    ? '<button class="wishItemPurchaseButton">구매하기</button>'
+                    : '<button class="wishItemProposalButton">가격제안</button>'
+                }
+                <button class="wishItemXButton">
+                  <i class="fa-solid fa-xmark"></i>
+                </button>
+              </span>
+            </div>
+            <div class="wishItemLower">
+              <p>${product.time}</p>
+              <span class="wishItemCount">
+                <span class="wishItemChattingCount">
+                  <i class="fa-regular fa-comment-dots"></i>
+                  ${product.detail.sub_data.split(" · ")[2].split(" ")[1]}
+                </span>
+                <span class="wishItemLikeCount">
+                  <i class="fa-regular fa-heart"></i>
+                  ${product.detail.sub_data.split(" · ")[3].split(" ")[1]}
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </li>
+      `;
+    });
+  });
+
+// 버튼 이벤트
 document.querySelectorAll(".wishItemViewMore").forEach((item) => {
   item.addEventListener("click", function (e) {
     e.preventDefault();
