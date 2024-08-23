2a14965022f04c08a0df17d8e98eae4f6e756c08
eazzie960904 <eazzie960904@gmail.com>
2024-08-23 19:52:54 +0900

    240823

--- scripts/myPages.js
+++ scripts/myPages.js
@@ -4,69 +4,69 @@ window.onload = function () {
 };
 
 // 탭 버튼에 카운트 삽입
-let wishItemArr = ["179612232", "179612037", "179611921"];
-const wishItems = document.querySelector(".wishItems");
-wishItems.innerText = wishItemArr.length;
+let mypageItemArr = ["19370577", "26753605", "104046947"];
+const mypageItems = document.querySelector(".mypageItems");
+mypageItems.innerText = mypageItemArr.length;
 
 // 탭 버튼 클릭 이벤트
-const wishlistTabButton = document.querySelectorAll(".wishlistTabButton");
-wishlistTabButton.forEach((btn) => {
+const mypageTabButton = document.querySelectorAll(".mypageTabButton");
+mypageTabButton.forEach((btn) => {
   btn.addEventListener("click", function () {
-    const wishlistContent = document.querySelectorAll(".wishlistContent");
+    const mypageContent = document.querySelectorAll(".mypageContent");
     const target = document.querySelector(
       `.${this.getAttribute("data-tab-name")}-content`
     );
 
     // 탭 버튼 초기화 및 가상클래스 적용
-    wishlistTabButton.forEach((sibling) => {
+    mypageTabButton.forEach((sibling) => {
       if (sibling !== this) sibling.classList.remove("active");
     });
     this.classList.add("active");
 
     // 탭 내용 초기화 및 가상클래스 적용
-    wishlistContent.forEach((item) => {
+    mypageContent.forEach((item) => {
       if (item !== target) item.classList.remove("active");
     });
     target.classList.add("active");
   });
 });
 
-// Putting Items in the wishItemList
-const addItemsInTheWishItemList = (product, index) => {
-  wishItemArr.forEach((arr) => {
+// Putting Items in the mypageItemList
+const addItemsInThemypageItemList = (product, index) => {
+  mypageItemArr.forEach((arr) => {
     if (arr === product.id) {
-      document.querySelector(".wishItemList").innerHTML += `
+      document.querySelector(".mypageItemList").innerHTML += `
       <li>
-        <input type="checkbox" name="checkWishItem" id="wishItem${
+        <input type="checkbox" name="checkmypageItem" id="mypageItem${
           index + 1
-        }" value="wishItem${index + 1}" />
-        <div class="wishItem">
-          <div class="wishItemImgWrapper">
-            <a href="#" class="wishItemViewMore">
+        }" value="mypageItem${index + 1}" />
+        <div class="mypageItem">
+          <div class="mypageItemImgWrapper">
+            <a href="#" class="mypageItemViewMore">
               <img
                 src="../${product.image_path}"
                 alt="product_image"
               />
             </a>
-            <button class="wishHeart">
+            <button class="mypageHeart">
               <i class="fa-solid fa-heart"></i>
             </button>
           </div>
-          <div class="wishItemDescGroup">
-            <div class="wishItemDesc">
-              <a href="#" class="wishItemViewMore">
-                <span class="wishItemInfo">
-                  <span class="wishItemTitleBox">
-                    <p class="wishItemSellerName">${
+          <div class="mypageItemDescGroup">
+            <div class="mypageItemDesc">
+              <a href="#" class="mypageItemViewMore">
+                <span class="mypageItemInfo">
+                  <span class="mypageItemTitleBox">
+                    <p class="mypageItemSellerName">${
                       product.detail.product_store
                     }</p>
-                    <p class="wishItemName">
+                    <p class="mypageItemName">
                       ${product.title}
                     </p>
                   </span>
-                  <span class="wishItemPriceBox">
-                    <span class="wishItemPriceInfo">
-                      <p class="wishItemPrice">
+                  <span class="mypageItemPriceBox">
+                    <span class="mypageItemPriceInfo">
+                      <p class="mypageItemPrice">
                         <b>${product.price.slice(0, -1)}</b>원
                       </p>
                       ${
@@ -75,8 +75,8 @@ const addItemsInTheWishItemList = (product, index) => {
                           : ""
                       }
                     </span>
-                    <span class="wishItemShippingInfo">
-                      <p class="wishItemShippingCharge">
+                    <span class="mypageItemShippingInfo">
+                      <p class="mypageItemShippingCharge">
                         ${
                           product.detail.delivery_charge === "-"
                             ? "직거래"
@@ -86,33 +86,33 @@ const addItemsInTheWishItemList = (product, index) => {
                       ${
                         product.point === ""
                           ? ""
-                          : `<span class="wishItemPlace"><i class="fa-solid fa-location-dot"></i> ${product.point}</span>`
+                          : `<span class="mypageItemPlace"><i class="fa-solid fa-location-dot"></i> ${product.point}</span>`
                       }
                       
                     </span>
                   </span>
                 </span>
               </a>
-              <span class="wishItemButtons">
-                <button class="wishItemChatButton">채팅하기</button>
+              <span class="mypageItemButtons">
+                <button class="mypageItemChatButton">채팅하기</button>
                 ${
                   product.pay_flag > 0
-                    ? '<button class="wishItemPurchaseButton">구매하기</button>'
-                    : '<button class="wishItemProposalButton">가격제안</button>'
+                    ? '<button class="mypageItemPurchaseButton">구매하기</button>'
+                    : '<button class="mypageItemProposalButton">가격제안</button>'
                 }
-                <button class="wishItemXButton">
+                <button class="mypageItemXButton">
                   <i class="fa-solid fa-xmark"></i>
                 </button>
               </span>
             </div>
-            <div class="wishItemLower">
+            <div class="mypageItemLower">
               <p>${product.time}</p>
-              <span class="wishItemCount">
-                <span class="wishItemChattingCount">
+              <span class="mypageItemCount">
+                <span class="mypageItemChattingCount">
                   <i class="fa-regular fa-comment-dots"></i>
                   ${product.detail.sub_data.split(" · ")[2].split(" ")[1]}
                 </span>
-                <span class="wishItemLikeCount">
+                <span class="mypageItemLikeCount">
                   <i class="fa-regular fa-heart"></i>
                   ${product.detail.sub_data.split(" · ")[3].split(" ")[1]}
                 </span>
@@ -127,28 +127,28 @@ const addItemsInTheWishItemList = (product, index) => {
 };
 
 // 데이터 추가
-/*fetch("../db.json")
+fetch("../db.json")
   .then((response) => response.json())
   .then((jsonData) => {
-    jsonData.data.forEach((product, index) => {
-      addItemsInTheWishItemList(product, index);
+    jsonData.product.forEach((product, index) => {
+      addItemsInThemypageItemList(product, index);
     });
-  });*/
+  });
 
 // 버튼 이벤트
-document.querySelectorAll(".wishItemViewMore").forEach((item) => {
+document.querySelectorAll(".mypageItemViewMore").forEach((item) => {
   item.addEventListener("click", function (e) {
     e.preventDefault();
     console.log(this);
   });
 });
-document.querySelectorAll(".wishHeart").forEach((item) => {
+document.querySelectorAll(".mypageHeart").forEach((item) => {
   item.addEventListener("click", function (e) {
     e.preventDefault();
     console.log(this);
   });
 });
-document.querySelectorAll(".wishItemChatButton").forEach((item) => {
+document.querySelectorAll(".mypageItemChatButton").forEach((item) => {
   item.addEventListener("click", function () {
     e.preventDefault();
     console.log(this);
