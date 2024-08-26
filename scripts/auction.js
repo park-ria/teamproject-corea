ba7a432c1d92ec4dc59731955a26522dd6a34a42
park-ria <danny6217@gmail.com>
2024-08-26 19:45:11 +0900

    240826 yeon slide완

--- scripts/auction.js
+++ scripts/auction.js
@@ -1,7 +1,22 @@
 const joonggoInfo = "../db.json";
 
 let slideIndex = 0;
-let slidesPerView = 3;
+//let slidesPerView = 3;
+/*let slidesPerView = matchMedia("screen and (min-width: 1280px)").matches
+  ? 3
+  : 2;*/
+
+if (matchMedia("screen and (min-width: 1280px)").matches) {
+  slidesPerView = 3;
+} else {
+  slidesPerView = 1;
+}
+// else if (matchMedia("screen and (min-width: 500px)").matches) {
+//   slidesPerView = 2;
+// } else if (matchMedia("screen and (max-width: 500px)").matches) {
+//   slidesPerView = 1;
+// }
+
 const productSlideLimit = 10;
 
 const addBestProduct = (product, ul) => {
@@ -11,24 +26,17 @@ const addBestProduct = (product, ul) => {
   const slideImg = document.createElement("div");
   const slideDesc = document.createElement("div");
   const badge = document.createElement("span");
-  let badgeClassName = "";
-  switch (ul) {
-    case ".bestRankingUl":
-      badgeClassName = "badge badge-best";
-      break;
-    case ".auctionUl":
-      badgeClassName = "badge badge-auction";
-      break;
-    case ".recommendedUl":
-      badgeClassName = "badge badge-new";
-      break;
-  }
-  badge.className = badgeClassName;
+  badge.className = "badge badge-auction";
 
-  slideImg.className = "slide-img";
-  slideDesc.className = "slide-desc";
   aTag.setAttribute("href", `/pages/detail.html?id=${product.id}`);
   // aTag.style.webkitUserDrag = "none";
+
+  slideImg.className = "slide-img";
+
+  const timeEvent = document.createElement("div");
+  timeEvent.className = "timeEvent";
+
+  slideDesc.className = "slide-desc";
 
   // <div class="timeEvent">
   //   <div class="clock"></div>
@@ -54,6 +62,7 @@ const addBestProduct = (product, ul) => {
         `;
 
   slideDesc.innerHTML = desc;
+  slideImg.appendChild(timeEvent);
   aTag.append(slideImg, slideDesc);
   liItem.appendChild(aTag);
   liItem.appendChild(badge);
@@ -79,8 +88,8 @@ const productSlide = (section) => {
   const pagers = slideSection.querySelectorAll(".slidePager span");
 
   const slideCount = slide.length;
-  const slideWidth = 410;
-  const slideMargin = 25;
+  // const slideWidth = 410;
+  // const slideMargin = 25;
 
   let currentIdx = 0;
   // move pager
@@ -90,6 +99,7 @@ const productSlide = (section) => {
       pager.classList.remove("active");
     }
     pagers[index].classList.add("active");
+    // console.log(index);
   };
 
   // click pager
@@ -105,23 +115,48 @@ const productSlide = (section) => {
     });
   });
 
+  // const moveSlide = (num) => {
+  //   currentIdx = num;
+  //   const keyIndex = slideCount - slidesPerView + 1;
+  //   const showIndex = (keyIndex + num) % keyIndex;
+  //   movePager(showIndex);
+
+  //   slideUl.style.left = `${-num * (slideWidth + slideMargin)}px`;
+
+  //   if (currentIdx === keyIndex) {
+  //     slideUl.style.left = 0;
+  //     currentIdx = 0;
+  //   }
+  //   if (currentIdx === -1) {
+  //     slideUl.style.left = `-${
+  //       (slideWidth + slideMargin) * (slideCount - slidesPerView)
+  //     }px`;
+  //     currentIdx = slideCount - slidesPerView;
+  //   }
+  // };
+
   const moveSlide = (num) => {
-    currentIdx = num;
-    const keyIndex = slideCount - slidesPerView + 1;
-    const showIndex = (keyIndex + num) % keyIndex;
-    movePager(showIndex);
+    if (num < 0 || num >= slideCount) return;
 
-    slideUl.style.left = `${-num * (slideWidth + slideMargin)}px`;
+    const slideWidth = slideUl.querySelectorAll("li>a")[0].offsetWidth;
+    const slideMargin = 25;
+    const currentSlideWidth = (slideCount - num) * (slideWidth + slideMargin);
+    const clientWidth = slideUl.parentElement.clientWidth;
 
-    if (currentIdx === keyIndex) {
-      slideUl.style.left = 0;
-      currentIdx = 0;
-    }
-    if (currentIdx === -1) {
-      slideUl.style.left = `-${
-        (slideWidth + slideMargin) * (slideCount - slidesPerView)
-      }px`;
-      currentIdx = slideCount - slidesPerView;
+    if (currentSlideWidth >= clientWidth) {
+      currentIdx = num;
+      movePager(currentIdx);
+      slideUl.style.transform = `translateX(${
+        -num * (slideWidth + slideMargin)
+      }px)`;
+    } else if (clientWidth - currentSlideWidth < slideWidth - slideMargin) {
+      currentIdx = num;
+      movePager(currentIdx);
+      slideUl.style.transform = `translateX(${
+        -(num - 1) * (slideWidth + slideMargin) -
+        slideWidth +
+        (clientWidth - currentSlideWidth)
+      }px)`;
     }
   };
 
@@ -138,6 +173,7 @@ const productSlide = (section) => {
 
   slideUl.addEventListener("mousedown", (e) => {
     slideUl.style.cursor = "grabbing";
+    // slide.querySelectorAll("a").style.cursor = "grabbing";
     startPoint = e.pageX;
   });
 
@@ -203,6 +239,12 @@ const addProductList = (product, ul) => {
   liItem.appendChild(badge);
   ulItem.appendChild(liItem);
 };
+
+// function mediaResize() {
+//   if (window.innerWidth < 450) {
+// }
+// }
+// window.addEventListener("resize", mediaResize);
 
 fetch(joonggoInfo)
   .then((response) => response.json())
