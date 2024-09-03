2ac4cac1173a86cc05a1b74c4c98673684fd93b7
hoon730 <ehdgns730@gmail.com>
2024-09-03 19:16:47 +0900

    240903

--- scripts/detail.js
+++ scripts/detail.js
@@ -370,38 +370,81 @@ fetch(joonggoInfo)
           : `<i class="fa-solid fa-location-dot"></i> ${product.point}`
       }`;
 
-      // Map API
-      const mapContainer = document.getElementById("map"), // 지도를 표시할 div
-        mapOption = {
-          center: new kakao.maps.LatLng(
-            product.detail.latitude,
-            product.detail.longitude
-          ), // 지도의 중심좌표
-          level: 3, // 지도의 확대 레벨
+      // // Map API
+      // const mapContainer = document.getElementById("map"), // 지도를 표시할 div
+      //   mapOption = {
+      //     center: new kakao.maps.LatLng(
+      //       product.detail.latitude,
+      //       product.detail.longitude
+      //     ), // 지도의 중심좌표
+      //     level: 3, // 지도의 확대 레벨
+      //   };
+
+      // const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+      // // 마커가 표시될 위치입니다
+      // const markerPosition = new kakao.maps.LatLng(
+      //   product.detail.latitude,
+      //   product.detail.longitude
+      // );
+
+      // // 마커를 생성합니다
+      // const marker = new kakao.maps.Marker({
+      //   position: markerPosition,
+      // });
+
+      // // 마커가 지도 위에 표시되도록 설정합니다
+      // marker.setMap(map);
+
+      // // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+      // const mapTypeControl = new kakao.maps.MapTypeControl();
+
+      // // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+      // // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+      // map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+      mapOption = { 
+            center: new kakao.maps.LatLng(product.detail.latitude, product.detail.longitude), // 지도의 중심좌표
+            level: 4 // 지도의 확대 레벨
         };
 
-      const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    var map = new kakao.maps.Map(mapContainer, mapOption);
 
-      // 마커가 표시될 위치입니다
-      const markerPosition = new kakao.maps.LatLng(
-        product.detail.latitude,
-        product.detail.longitude
-      );
+    var imageSrc = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt4FvOAHbeZEguySzawWHdM_4XZ9HZ9HxtBA&s', // 마커이미지의 주소입니다    
+        imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+        imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
-      // 마커를 생성합니다
-      const marker = new kakao.maps.Marker({
-        position: markerPosition,
-      });
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+        markerPosition = new kakao.maps.LatLng(product.detail.latitude, product.detail.longitude); // 마커가 표시될 위치입니다
 
-      // 마커가 지도 위에 표시되도록 설정합니다
-      marker.setMap(map);
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      position: markerPosition,
+      image: markerImage // 마커이미지 설정 
+    });
 
-      // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-      const mapTypeControl = new kakao.maps.MapTypeControl();
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);  
 
-      // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-      // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
-      map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+    // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    var content = '<div class="customoverlay">' +
+        '  <a href="https://map.kakao.com/link/map/11394059" target="_blank">' +
+        '    <span class="title">구의야구공원</span>' +
+        '  </a>' +
+        '</div>';
+
+    // 커스텀 오버레이가 표시될 위치입니다 
+    var position = new kakao.maps.LatLng(37.54699, 127.09598);  
+
+    // 커스텀 오버레이를 생성합니다
+    var customOverlay = new kakao.maps.CustomOverlay({
+        map: map,
+        position: position,
+        content: content,
+        yAnchor: 1 
+    });
 
       // trade버튼 클릭시 이벤트
       const tradeBtn = document.querySelector(".trade");
